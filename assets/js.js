1ba5691619cace0f7f7ec4d192f5d5b6fd7efa96
Shaun Sullivan <shaunsully88@gmail.com>
2022-03-15 14:49:55 -0400

    Questions changed from HTML format to Array format

--- assets/js.js
+++ assets/js.js
@@ -1,13 +1,44 @@
 // timer countdown //
-var timerEl = document.getElementById('timer');
-var secs = 0;
+var timerEl = document.querySelector("timer");
 var timeLeft= 75;
 
+//Highscore//
+var highScoreEl = document.querySelector("#highscore");
+
+//Starter Page//
+
+var startPageEl = document.querySeleector("#startPage");
+
+var questions = [
+   {
+      questionText: "Commonly used data types do not include:",
+      answers: ["1. Strings", "2. Booleans", "3. Alerts", "4. Numbers"],
+      correct: "2"
+   },
+   {
+      questionText: "The condition in an if/else statement is enclosed with _____.",
+      answers: ["1. Quotes", "2. Curly Brackets", "3. Parenthesis", "4. Square Brackets"],
+      correct: "1"
+   },
+   {
+      questionText: "Arrays in Javascript can be used to store _____." ,
+      answers: ["1. Numbers and Strings", "2. Other Arrays", "3. Booleans", "4. All of the above"],
+      correct: "3"
+   }  
+   {
+      questionText: " String Values must be enclosed in within ____ when being assigned to variables.",
+      answers: ["1. Commas", "2. Curly Brackets", "3. Quotes", "4. Parenthesis"]
+   }
+
+]
+
+
 function countdown() {
-    
+
+
     var timeInterval = setInterval(function() {
         timeLeft--; console.log(timeLeft);
-        timerEl.innerHTML = "Time Left <br/>" + timeLeft;
+        timerEl.textContent = "Time Left <br/>" + timeLeft;
 
         if(timeLeft === 0) {
             clearInterval(timeInterval);
@@ -16,5 +47,12 @@ function countdown() {
     }, 1000
     );
 };    
+
+function startQuiz() {
+    startPage
+
+    countdown();
+
+}
 
 countdown();
